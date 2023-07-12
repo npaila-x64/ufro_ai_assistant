8d42a18f82279ae17fc81506ca6907357f085704
Nicolás Paila <n.paila01@ufromail.cl>
2023-07-11 23:10:55 -0400

    add simple token usage logging

--- query_service/index_constructor.py
+++ query_service/index_constructor.py
@@ -1,3 +1,4 @@
+import tiktoken
 from llama_index import (
     SimpleDirectoryReader,
     VectorStoreIndex,
@@ -5,7 +6,10 @@ from llama_index import (
     LLMPredictor,
     set_global_service_context
 )
-
+from llama_index.callbacks import (
+    CallbackManager, 
+    TokenCountingHandler
+)
 from llama_index.node_parser import SimpleNodeParser
 from langchain.chat_models import ChatOpenAI
 import openai
@@ -25,16 +29,33 @@ def construct_index():
 
     nodes = parser.get_nodes_from_documents(documents)
 
+    # setup callback manager to monitor token usage
+    token_counter = TokenCountingHandler(
+        tokenizer=tiktoken.encoding_for_model("gpt-3.5-turbo").encode
+    )
+    callback_manager = CallbackManager([token_counter])
+
     # define LLM
     llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=max_tokens))
 
     # configure service context
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+    service_context = ServiceContext.from_defaults(
+        llm_predictor=llm_predictor, 
+        callback_manager=callback_manager
+    )
 
+    # set the current service as the global service
+    # this will be the service used if not explicitly specified
     set_global_service_context(service_context)
 
     # build index
     index = VectorStoreIndex.from_documents(documents)
+
+    print('Embedding Tokens: ', token_counter.total_embedding_token_count, '\n',
+      'LLM Prompt Tokens: ', token_counter.prompt_llm_token_count, '\n',
+      'LLM Completion Tokens: ', token_counter.completion_llm_token_count, '\n',
+      'Total LLM Token Count: ', token_counter.total_llm_token_count, '\n')
+    token_counter.reset_counts()
 
     index.storage_context.persist(persist_dir="persist")
 
